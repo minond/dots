a2ada833dc863cbe8522d7ce0d4e07abe50d3fdd
Marcos Minond <minond.marcos@gmail.com>
2014-04-04 23:04:13 -0600

    cleaned up grunt files

--- scaffolding/Gruntfile.js
+++ scaffolding/Gruntfile.js
@@ -1,47 +1,36 @@
-// http://www.thomasboyt.com/2013/09/01/maintainable-grunt.html
+/**
+ * no need to edit this file. configured by config/build.yml
+ * @link http://www.thomasboyt.com/2013/09/01/maintainable-grunt.html
+ */
 module.exports = function (grunt) {
     'use strict';
 
-    var glob, config, tasks, task, path;
+    var _, glob, config, tasks, task;
 
+    _ = require('lodash');
     glob = require('glob');
     config = grunt.file.readYAML('config/build.yml'),
-    path = config.files.tasks.options.dir;
     tasks = { config: config };
 
-    glob.sync('*', { cwd: path }).forEach(function (option) {
-        task = option.replace(/\.js$/,'');
-        tasks[ task ] = require(path + option);
+    // configs
+    _(config.imports).each(function (file) {
+        config = _.defaults(config,
+            grunt.file.readYAML('config/build.yml'));
+    });
+
+    // options
+    _(config.options).each(function (path) {
+        glob.sync('*.js', { cwd: path }).forEach(function (option) {
+            task = option.replace(/\.js$/,'');
+            tasks[ task ] = require(path + option);
+        });
     });
 
     grunt.initConfig(tasks);
     require('load-grunt-tasks')(grunt);
 
-    grunt.registerTask('default', [ 'build' ]);
-    grunt.registerTask('prepare', [ 'mkdir:build' ]);
-    grunt.registerTask('clean', [ 'rm:build' ]);
-    grunt.registerTask('compile', [ 'sass:all' ]);
-    grunt.registerTask('server', [ 'connect:server' ]);
-
-    grunt.registerTask('build', [ 'prepare', 'clean', 'prepare', 'compile' ]);
-    grunt.registerTask('documentation', [ 'yuidoc:all', 'exec:apigen' ]);
-    grunt.registerTask('ci', [ 'quality', 'test', 'documentation' ]);
-
-    grunt.registerTask('test', [ 'test:unit', 'test:integration' ]);
-    grunt.registerTask('test:integration', [ 'prepare', 'exec:behat' ]);
-    grunt.registerTask('test:unit', [ 'test:unit:js', 'test:unit:php' ]);
-    grunt.registerTask('test:unit:js', [ 'jasmine:all' ]);
-    grunt.registerTask('test:unit:php', [ 'phpunit:all' ]);
-
-    grunt.registerTask('quality', [
-        'prepare',
-        'complexity:all',
-        'jshint:all',
-        'jshint:report',
-        'exec:scsslint',
-        'exec:phpcpd',
-        'exec:phpcpd',
-        'exec:phpmd',
-        'phpcs:all'
-    ]);
+    // tasks
+    _(config.aliases).forOwn(function (tasks, alias) {
+        grunt.registerTask(alias, tasks);
+    });
 };
